5f0a74f685f04afb1f13bc347da236205f9a40bf
Gurjot Singh <gurjotssidhu08@gmail.com>
2018-01-12 00:31:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,12 @@
 function forLoop(array){
-if()
+let i = 1;
+
+if(i === 1){
+return "I am 1 strange loop";
+}else{
+  
+for(let i = 2; i < 25; i++)
+console.log("I am ${i} strange loops.")
+  
+}
 }
