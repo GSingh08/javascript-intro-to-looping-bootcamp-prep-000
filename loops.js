b5133c45a636bb2d0d814232a0209af889ee37ae
Gurjot Singh <gurjotssidhu08@gmail.com>
2018-01-12 00:33:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,8 @@
 function forLoop(array){
-let i = 1;
-
-if(i === 1){
-return "I am 1 strange loop";
-}else{
   
-for(let i = 2; i < 25; i++)
-console.log("I am ${i} strange loops.")
+for(var i = 1; i < 25; i++){
   
 }
+
+
 }
